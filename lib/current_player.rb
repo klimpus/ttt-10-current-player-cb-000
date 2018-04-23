42d0b43947d9e0747e8e051b17a15cb1fa5b0e58
Kristopher Limpus <krislimpus@gmail.com>
2018-04-23 22:48:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def turn_count(board)
   counter = 0
   board.each do |spaces|
@@ -13,6 +14,26 @@ def current_player(board)
     return "X"
   elsif turn_count(board)
     return "O"
+=======
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def turn_count(board)
+counter = 0
+board.each do |index|
+if index == "X" || index == "O"
+    counter += 1
+    return counter
+end
+end
+end
+
+
+
+def current_player(board)
+  if turn_count(board).even?
+    return "O"
+  else
+    return "X"
+>>>>>>> 462e7c98bc4f36d44e708a79a1c4bdba3572dc61
 end
 end
 
